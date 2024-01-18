0161785b1290410abaab7993d6f418083243c35c
Aisha-Moh <124839710+Aisha-Moh@users.noreply.github.com>
2024-01-18 14:45:47 +0000

    completed 9-get-api-users

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const {
   postCommentByArticleId,
   deleteCommentByCommentId,
 } = require("./controllers/comments.controller");
+const { getUsers } = require("./controllers/users.controller");
 
 const app = express();
 
@@ -30,6 +31,8 @@ app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 app.patch("/api/articles/:article_id", patchArticleByArticleId);
 
 app.delete("/api/comments/:comment_id", deleteCommentByCommentId);
+
+app.get("/api/users", getUsers);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "endpoint not found" });
